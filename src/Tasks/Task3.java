ca59c01323a09de7c6a51f99106c427934da765b
piokowalski <piokowalski@onet.pl>
2019-03-27 19:58:18 +0100

    Task 3 - palindrome is working

--- src/Tasks/Task3.java
+++ src/Tasks/Task3.java
@@ -1,10 +1,24 @@
 package Tasks;
-
-
-//palindrom task
-
+import java.util.Scanner;
 public class Task3 {
     public void run() {
+        String txtA, txtB = "";
+        System.out.println("Enter the text to palindrome check:");
+        Scanner scanner = new Scanner(System.in);
+
+        txtA = scanner.nextLine();
+
+
+        int a =txtA.length();
+
+        for(int i=a-1; i >= 0; i--) {
+            txtB = txtB + txtA.charAt(i);
+        }
+        if (txtA.equalsIgnoreCase(txtB)) {
+            System.out.println("This is a palindrome!");
+        } else {
+            System.out.println("Not today, SATAN.");
+        }
 
     }
 }

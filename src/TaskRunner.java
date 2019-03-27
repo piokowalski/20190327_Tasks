19597f8a51324040bc387ba2112a98929748cfdc
piokowalski <piokowalski@onet.pl>
2019-03-27 16:53:45 +0100

    Task 3 - palindrome - starting this task

--- src/TaskRunner.java
+++ src/TaskRunner.java
@@ -1,5 +1,6 @@
 import Tasks.Task1;
 import Tasks.Task2;
+import Tasks.Task3;
 
 public class TaskRunner {
     public void run() {
@@ -7,11 +8,11 @@ public class TaskRunner {
 //        Task1 task1 = new Task1();
 //        task1.run();
 
+//        Task2 task2 = new Task2();
+//        task2.run();
 
-        Task2 task2 = new Task2();
-        task2.run();
-
-
+        Task3 task3 = new Task3();
+        task3.run();
 
 
     }
---
+++ src/Tasks/Task3.java
@@ -0,0 +1,10 @@
+package Tasks;
+
+
+//palindrom task
+
+public class Task3 {
+    public void run() {
+
+    }
+}
